9f2279138db032cf5c58629f2cf96b8993267ae8
Rosario <rosario@roundabout.io>
2015-12-09 14:58:46 +0000

    1) render a static page, 2) render a JSON, 3) load AngularJS from CDN

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var pass = require('./passwd.json')
 
 var app = express();
 
-console.log(pass)
+var bodyParser = require('body-parser')
+
+app.use(express.static('public'));
+
+app.set('port', 3000);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res){
+    res.render('index');
+});
+
+app.get('/user', function(req, res){
+    res.status(200).send({ name: 'toto' })
+});
+
+
 module.exports = app;
 
